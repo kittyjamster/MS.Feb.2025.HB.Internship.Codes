bee97117e87c50ea99fd5675f06f0698ff7999f4
Mikayla <157250459+kittyjamster@users.noreply.github.com>
2025-02-08 00:04:18 -0500

    Add files via upload

---
+++ stage_0_folder/stage0-task-tryptophan-R-script.R
@@ -0,0 +1,32 @@
+#stage 0 task: structure data of all team members without using complex concepts (i.e. functions, loops, conditionals, etc.)
+#data included: Names, Slack.UN, email address, hobby, countries, discipline, preferred programming language
+
+#opted for data frame format (alternative could be a matrix)
+my.team <- data.frame(
+  Name = c ("Mikayla Sheild", "Valentim", "Josiah", "Happiness"),
+  Slack.UN = c("Mikayla", "José Valentim", "Josiah", "Happiness Ezinna"),
+  email = c("msheild.1218@gmail.com", "jose.valentimfilhoUK@gmail.com", "josiaholubanjo@gmail.com", "ezinnahappiness11@gmail.com"),
+  Country = c("The US", "Brazil", "Nigeria", "Nigeria"),
+  Discipline = c("Genetics & Genomics", "Electronic Engineering", "Biochemistry", "Molecular Parisitology"),
+  Hobbies = c("Reading & writing", "INFO HERE", "Reading", "Reading & Singing"),
+  LanguagePref = c( "R", "Python", "Python", "INFO HERE") #LanguagePref refers to preferred programming language
+)
+
+#printing my.team info
+print(my.team)
+
+#had the time to include an alternate format
+#alternative structure in matrix format
+alt_team_form <- matrix (c("Mikayla", "msheild.1218@gmail.com", "TheUS", "Genetics&Genomics", "Reading&Writing", "R", 
+                           "José Valentim", "jose.valentimfilhoUK@gmail.com", "Brazil", "ElectronicEngineering", "INFO HERE", "Python",
+                           "Josiah", "josiaholubanjo@gmail.com", "Nigeria", "Biochemistry", "Reading", "Python",
+                           "Happiness Ezinna", "ezinnahappiness11@gmail.com", "Nigeria", "Molecular Parisitology", "Reading&Singing", "INFO HERE"), nrow=6, ncol=4)
+
+#team members' names are serving as column names
+colnames(alt_team_form) <- c("Mikayla Sheild", "Valentim", "Josiah", "Happiness")
+rownames(alt_team_form) <- c("Slack.UN", "Email", "Country", "Discipline", "Hobbies", "LanguagePref")
+
+#Printing matrix format
+print(alt_team_form)
+
+#Mikayla's github can be found here: https://github.com/kittyjamster/MS.Feb.2025.HB.Internship.Codes 
